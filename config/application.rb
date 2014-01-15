c489032a1ad543aed97cc2a988e20ad65e79c420
Matt Atkins <matt@yoomee.com>
2014-01-15 14:14:02 +0000

    Remove gem migration includes

--- config/application.rb
+++ config/application.rb
@@ -33,8 +33,6 @@ module MasStudentMoneyTips
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.paths['db/migrate'] = StudentMoneyTips::Engine.paths['db/migrate'].existent
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
