b5d7010913308ea8059dd96eb0b6484e7a6a4c3d
Matt Atkins <matt@yoomee.com>
2014-01-16 15:21:20 +0000

    Add tool seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 require 'yaml'
-scenarios = YAML.load_file('db/seeds/scenarios.yml')
-scenarios.each do |scenario|
+
+tools_data = YAML.load_file('db/seeds/tools.yml')
+tools = {}
+tools_data.each do |tool|
+  tools[tool["code"]] = StudentMoneyTips::Tool.create(:title_en => tool["title"], :link_text_en => 'Go to the tool', :url => tool["url"])
+end
+
+scenarios_data = YAML.load_file('db/seeds/scenarios.yml')
+scenarios_data.each do |scenario|
   s = StudentMoneyTips::Scenario.new(:name_en => scenario["name"])
   scenario["tips"].each do |tip|
     s.tips.build(:text_en => tip)
+  end
+  scenario["tools"].each do |code|
+    s.tools << tools[code]
   end
   s.save
 end
