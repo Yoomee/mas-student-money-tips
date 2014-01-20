0e626b7e00ae05cbcf8633be3af25a005e7a1485
Matt Atkins <matt@yoomee.com>
2014-01-20 15:19:48 +0000

    Generate scenarios.yml from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'csv'
 
 tools_data = YAML.load_file('db/seeds/tools.yml')
 tools = {}
@@ -6,7 +7,38 @@ tools_data.each do |tool|
   tools[tool["code"]] = StudentMoneyTips::Tool.create(:title_en => tool["title"], :link_text_en => 'Go to the tool', :url => tool["url"])
 end
 
-scenarios_data = YAML.load_file('db/seeds/scenarios.yml')
+
+scenarios_yaml = 'db/seeds/scenarios.yml'
+unless File.exists?(scenarios_yaml)
+  tool_codes = [
+    %w{HC BP LC CT DT},
+    %w{HC BP CT DT},
+    %w{SMC HC BP CT DT},
+    %w{BP HC LC DT},
+    %w{SMC BP HC CT DT},
+    %w{SMC BP HC LC CT DT},
+    %w{HC BP LC CT DT},
+    %w{HC BP CT DT}
+  ]
+
+  rows = CSV.new(File.read('db/seeds/tips_scenarios.csv')).to_a
+
+  scenarios = rows.shift.compact.collect{|s| {'name' => s.downcase, 'tips' => []}}
+
+  rows.each do |row|
+    tip = row.shift
+    scenarios.each_with_index do |scenario, idx|
+      if row[idx].to_s.strip.length > 0
+        scenario['tips'] << tip
+      end
+      scenario['tools'] = tool_codes[idx]
+    end
+  end
+  
+  File.open(scenarios_yaml, 'w') {|f| f.puts scenarios.to_yaml}
+end
+
+scenarios_data = YAML.load_file(scenarios_yaml)
 scenarios_data.each do |scenario|
   s = StudentMoneyTips::Scenario.new(:name_en => scenario["name"])
   scenario["tips"].each do |tip|
