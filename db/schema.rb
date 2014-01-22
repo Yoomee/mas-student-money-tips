5ca59cb03503b274f29ab45e4e830a5cb39cbddd
Matt Atkins <matt@yoomee.com>
2014-01-22 11:58:12 +0000

    Update gem for audit trail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140121154109) do
+ActiveRecord::Schema.define(:version => 20140122094102) do
+
+  create_table "student_money_tips_audit_records", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "resource_type"
+    t.integer  "resource_id"
+    t.string   "action"
+    t.datetime "created_at"
+  end
+
+  add_index "student_money_tips_audit_records", ["resource_type", "resource_id"], :name => "index_student_money_tips_audit_records_on_resource"
 
   create_table "student_money_tips_scenarios", :force => true do |t|
     t.string   "name_en"
