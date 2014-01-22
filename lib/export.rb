3183b0927fd681ed3d2fb622867f70ec38e2c423
Matt Atkins <matt@yoomee.com>
2014-01-22 12:13:42 +0000

    Don't export CSS, now pulled from staging server

--- lib/export.rb
+++ lib/export.rb
@@ -2,8 +2,6 @@ system "mkdir -p export"
 system "rm -rf export/*"
 system "wget -O export/widget.html http://localhost:3000/en/student_money_tips/embed"
 system "wget -O export/results.html http://localhost:3000/en/student_money_tips/results"
-system "mkdir export/assets"
-system "wget -O export/assets/widget.css http://localhost:3000/assets/student_money_tips/widget.css"
 
 %w{widget results}.each do |file|
   path = "export/#{file}.html"
